aaaec5f071c0ef093f15556f3946b51476c07493
jhyeong <hyeongcode@gmail.com>
2024-11-12 09:07:34 +0900

    init: init

---
+++ src/main/java/org/esnack24api/esnack24api/ESnack24ApiApplication.java
@@ -0,0 +1,13 @@
+package org.esnack24api.esnack24api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ESnack24ApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ESnack24ApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/esnack24api/esnack24api/ESnack24ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.esnack24api.esnack24api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ESnack24ApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
