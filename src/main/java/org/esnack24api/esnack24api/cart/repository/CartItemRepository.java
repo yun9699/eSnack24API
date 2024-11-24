1d7007155dc0f06f345109c0436a1fef2cffafc8
jhyeong <hyeongcode@gmail.com>
2024-11-24 16:22:40 +0900

    clore: add CartITem bug fix

--- src/main/java/org/esnack24api/esnack24api/cart/repository/CartItemRepository.java
+++ src/main/java/org/esnack24api/esnack24api/cart/repository/CartItemRepository.java
@@ -2,10 +2,15 @@ package org.esnack24api.esnack24api.cart.repository;
 
 import org.esnack24api.esnack24api.cart.domain.CartEntity;
 import org.esnack24api.esnack24api.cart.domain.CartItemEntity;
+import org.esnack24api.esnack24api.product.domain.ProductEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
 
 
 public interface CartItemRepository extends JpaRepository<CartItemEntity, Long> {
 
     void deleteAllByCart(CartEntity cart);
+
+    Optional<CartItemEntity> findByProduct(ProductEntity product);
 }
