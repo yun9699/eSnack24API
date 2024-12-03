0d39d289e5deaaeabe1bd359d44b1ec3659899ad
jhyeong <hyeongcode@gmail.com>
2024-12-03 11:36:35 +0900

    feat: order detail add

--- src/main/java/org/esnack24api/esnack24api/order/controller/OrderController.java
+++ src/main/java/org/esnack24api/esnack24api/order/controller/OrderController.java
@@ -3,11 +3,14 @@ package org.esnack24api.esnack24api.order.controller;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.esnack24api.esnack24api.order.dto.CreateOrderDTO;
+import org.esnack24api.esnack24api.order.dto.OrderDetailDTO;
 import org.esnack24api.esnack24api.order.dto.OrderViewDTO;
 import org.esnack24api.esnack24api.order.service.OrderService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/order")
@@ -29,5 +32,11 @@ public class OrderController {
     public ResponseEntity<OrderViewDTO> viewOrder(@PathVariable Long ono) {
 
         return ResponseEntity.ok(orderService.viewOrder(ono));
+    }
+
+    @GetMapping("detail/{ono}")
+    public ResponseEntity<List<OrderDetailDTO>> orderDetail(@PathVariable Long ono) {
+
+        return ResponseEntity.ok(orderService.orderDetail(ono));
     }
 }
