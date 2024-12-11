ec1a75b40d9836ccc566f10949dc3a316b599d59
jhyeong <hyeongcode@gmail.com>
2024-12-11 14:06:11 +0900

    clore: HealthCheckController rename

--- src/main/java/org/esnack24api/esnack24api/common/controller/HealthCheckController.java
+++ src/main/java/org/esnack24api/esnack24api/common/controller/HealthCheckController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 @Log4j2
 @RequiredArgsConstructor
 @PreAuthorize("permitAll()")
-public class HeathCheckController {
+public class HealthCheckController {
 
     @GetMapping("")
     public String healthCheckController() {
