c382a994efcdfd1cfafd001cdd93a5e88aa764bf
jhyeong <hyeongcode@gmail.com>
2024-12-11 14:04:18 +0900

    feat: healthCheck controller add

---
+++ src/main/java/org/esnack24api/esnack24api/common/controller/HeathCheckController.java
@@ -0,0 +1,22 @@
+package org.esnack24api.esnack24api.common.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/")
+@Log4j2
+@RequiredArgsConstructor
+@PreAuthorize("permitAll()")
+public class HeathCheckController {
+
+    @GetMapping("")
+    public String healthCheckController() {
+
+        return "Health Check Complete";
+    }
+}
